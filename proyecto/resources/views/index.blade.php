1ce34e77b046e0a9fcc716d8632f594155212674
RubenGarciaGonzalez <rubengg_00@hotmail.com>
2020-02-23 13:11:01 +0100

    Cambios Pagina Inicio

---
+++ proyecto/resources/views/index.blade.php
@@ -0,0 +1,106 @@
+@extends('plantilla.plantilla')
+@section('titulo')
+    CodeFinder
+@endsection
+@section('contenido')
+<nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">CodeFinder</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="sr-only">CodeFinder</span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Buscar Código</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lenguajes</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            Perfil
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Iniciar Sesión</a>
+                            <a class="dropdown-item" href="#">Cerrar Sesión</a>
+                        </div>
+                    </li>
+                </ul>
+
+                <form class="form-inline ml-auto">
+                    <div class="form-group no-border">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-white btn-just-icon btn-round">
+                        <i class="material-icons">search</i>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="page-header header-filter" data-parallax="true" style="background-image: url('{{asset('img/programming.jpg')}}')">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 ml-auto mr-auto">
+                    <div class="brand text-center animated tada">
+                        <img src="{{asset('img/code.png')}}" width="100px" height="100px">
+                        <h1 id="titulo">CodeFinder</h1>
+                        <h3 class="title text-center" id="titulo" style="font-style: italic">Tu biblioteca de código</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main main-raised">
+        <div class="container">
+            <div class="flow-lg-row">
+                    <p class="h1 text-center my-2 font-italic">Encuentra. Comparte. Guarda</p>
+                    <blockquote class="blockquote text-center">
+                        <p class="mb-0">
+                            CodeFinder es un portal web creado y diseñado para los programadores. Creado por un alumno
+                            del grado Desarrollo de Aplicaciones Web, esta aplicación intenta servir de ayuda tanto, a 
+                            programadores que acaban de empezar en el gremio, como a los más veteranos. 
+                        </p>
+                    </blockquote>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer footer-default">
+        <div class="container">
+            <nav class="float-left">
+                <ul>
+                    <li>
+                        <a href="https://github.com/RubenGarciaGonzalez">
+                            Ruben Garcia 
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright float-right">
+                &copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>, hecho con <i class="material-icons">favorite</i> por
+                <a href="https://github.com/RubenGarciaGonzalez" target="blank">Ruben Garcia</a>.
+            </div>
+        </div>
+    </footer>
+    <script>
+        window.onload = function () {
+            Sticker.init('.sticker');
+        }
+    </script>
+@endsection
---
+++ proyecto/resources/views/plantilla/plantilla.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Enlaces necesairos para la funcionalidad de la página-->
+    <link href="{{asset('material/assets/css/material-kit.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('node_modules/animate.css/animate.min.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:500&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="{{asset('sticker.min.js')}}"></script>
+    <title>@yield('titulo')</title>
+</head>
+<body>
+    @yield('contenido')
+
+    <!--Scripts necesarios para la funcionalidad de la página-->
+    <script src="{{asset('material/assets/js/core/jquery.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('material/assets/js/core/popper.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('material/assets/js/core/bootstrap-material-design.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('material/assets/js/plugins/moment.min.js')}}"></script>
+    <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+    <script src="{{asset('material/assets/js/plugins/bootstrap-datetimepicker.js')}}" type="text/javascript"></script>
+    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+    <script src="{{asset('material/assets/js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
+    <!--  Google Maps Plugin  -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+    <script src="{{asset('material/assets/js/material-kit.js')}}" type="text/javascript"></script>
+</body>
+</html>
